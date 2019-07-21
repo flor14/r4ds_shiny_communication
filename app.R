0793719ac165755b78778441fb0d465916a6cedc
Florencia <florencia.dandrea@gmail.com>
2019-07-21 12:59:40 -0300

    fiesta

--- app.R
+++ app.R
@@ -22,62 +22,71 @@ ui <- fluidPage(
                theme = shinytheme("paper"),
                tabPanel("Labels",
     sidebarLayout(
-        div(style = "font-size: 10px; padding: 14px 0px; margin:0%",
-        sidebarPanel(tags$h6("library(ggplot2)", style = "padding: 0px 0px; margin-top:0em"), br(), 
-                     tags$h6("ggplot(mpg, aes(displ, hwy)) + ", style = "padding: 0px 0px; margin-top:0em"), br(), 
-                     tags$h6("geom_point(aes(color = class)) +", style = "padding: 0px 0px; margin-top:-1em"), br(),
-                     tags$h6("geom_smooth(se = FALSE) +", style = "padding: 0px 0px; margin-top:-1em"), br(),   
-                     tags$h6("lab(", style = "padding: 0px 0px; margin-top:-1em"), br(),       
+        div(style = "font-size: 13px; padding: 14px 0px; margin:0%",
+        sidebarPanel(p("library(ggplot2)", style = "padding: 0px 0px; margin-top:0em"), br(), 
+                     p("ggplot(mpg, aes(displ, hwy)) + ", style = "padding: 0px 0px; margin-top:0em"), br(), 
+                     p("geom_point(aes(color = class)) +", style = "padding: 0px 0px; margin-top:-3em"), br(),
+                     p("geom_smooth(se = FALSE) +", style = "padding: 0px 0px; margin-top:-3em"), br(),   
+                     p("labs(", style = "padding: 0px 0px; margin-top:-3em"), br(),       
             textInput("title", "title =", value = "Fuel efficiency generally decreases with engine size"),
             textInput("subtitle", "subtitle =", value = "Fuel efficiency generally decreases with engine size"),
             textInput("caption", "caption =", value = "Data from fueleconomy.gov"),
             textInput("x", "x =", value = "Engine displacement (L)"),
             textInput("y", "y =", value = "Highway fuel economy (mpg)"),
             textInput("colour", "colour =", value = "Car type"),
-            h6(")")
+            p(")", style = "padding: 0px 0px; margin-top:0em")
         ) #sidebarpanel
         ), #div
         mainPanel(
-            h5("Proba cambiar las labels de este gráfico"),
+            h5("Proba cambiar las etiquetas de este gráfico"),
+            p("Para usar símbolos se agrega la función 'quote()'. Ver ?plotmath."),
            plotOutput("gra28_lab"))#mainpanel
         ) #sidebarlayout
         ), #tabpanel
     tabPanel("Annotations", 
              sidebarLayout(
-                 div(style = "font-size: 10px; padding: 14px 0px; margin:0%",
+                 div(style = "font-size: 13px; padding: 0px 0px; margin:0%",
               sidebarPanel(
-                  tags$h6("best_in_class <- mpg %>%", style = "padding: 0px 0px; margin-top:0em"), br(),
-                  tags$h6("group_by(class) %>%", style = "padding: 0px 0px; margin-top:-2em"), br(), 
-                  tags$h6("filter(row_number(desc(hwy)) == 1)", style = "padding: 0px 0px; margin-top:-2em"), br(), 
-                  tags$h6("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:-1em"), br(), 
-                  tags$h6("geom_point(aes(colour = class)) +", style = "padding: 0px 0px; margin-top:-2em"), br(), 
-                  selectInput("graph", "", choices = c("geom_text", "geom_label", "geom_label_repel"), selected = "geom_text"),
-                  tags$h6("(aes(label = model), data = best_in_class, ", style = "padding: 0px 0px; margin-top:-0.5em"),
-                  numericInput("nudge_x", "nudge_x =", value = 0),
-                  numericInput("nudge_y", "nudge_y =", value = 2),
-                  sliderInput("alpha", "alpha =", min = 0, max = 1, value = 0.5),
-                  numericInput("size", "size =", value = 6),
-                  numericInput("label_size", "label.size =", value = 0),
-                 # checkboxInput("seg_col", "segment.color =", value = NA),
-                  selectInput("hjust", "hjust = ", choices = c("top", "center", "bottom"), selected = "center"),
-                  selectInput("vjust", "vjust = ", choices = c("left", "center", "right"), selected = "center")
+                  p("library(dplyr)", style = "padding: 0px 0px; margin-top:0em; color:green"),br(), 
+                  p("# Selecciona el auto más eficiente de su clase", style = "padding: 0px 0px; margin-top:0em"),br(), 
+                  p("best_in_class <- mpg %>%", style = "padding: 0px 0px; margin-top:-3em"), br(),
+                  p("group_by(class) %>%", style = "padding: 0px 0px; margin-top:-3em"), br(), 
+                  p("filter(row_number(desc(hwy)) == 1)", style = "padding: 0px 0px; margin-top:-3em"), br(), 
+                  p("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:-1em"), br(), 
+                  p("geom_point(aes(colour = class)) +", style = "padding: 0px 0px; margin-top:-3em"), br(), 
+                  selectInput("graph", choices = c("geom_text", "geom_label", "ggrepel::geom_label_repel"), selected = "geom_text", label = NULL),
+                  p("(aes(label = model), data = best_in_class, ", style = "padding: 0px 0px; margin-top:-0.5em"),
+                  numericInput("nudge_x", "nudge_x =", value = 0, width = 100),
+                  numericInput("nudge_y", "nudge_y =", value = 0, width = 100),
+                  sliderInput("alpha", "alpha =", min = 0, max = 1, value = 1),
+                  numericInput("size", "size =", value = 6, width = 100),
+                  numericInput("label_size", "label.size =", value = 0, width = 100),
+                  #checkboxInput("seg_col", "segment.color =", value = "NA"),
+                  selectInput("hjust", "hjust = ", choices = c("top", "center", "bottom"), selected = "center", width = 100),
+                  selectInput("vjust", "vjust = ", choices = c("left", "center", "right"), selected = "center", width = 100),
+                  p(")", style = "padding: 0px 0px; margin-top:0em")
                  ) #sidebarpanel
                  ), #div
               mainPanel( 
-                h6("Añadiendo texto a mis puntos. ¿Qué tiene de especial la función ggrepel::geom_label_text?"),
+                p("Añadiendo etiquetas a mis puntos."), br(),
+                p("Otros: geom_hline(), geom_vline(), geom_rect(), geom_segment()"),
                 plotOutput("selected_graph")
                 )# mainpanel
              ) #sidebarlayout
        ),#tabpanel
     tabPanel("Scales", #https://stackoverflow.com/questions/44159168/how-to-style-an-single-individual-selectinput-menu-in-r-shiny
              sidebarLayout(
-               div(style = "font-size: 10px; padding: 14px 0px; margin:0%",
+               div(style = "font-size: 13px; padding: 14px 0px; margin:0%",
                    sidebarPanel(
-                     tags$h6("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:-1em"), br(), 
-                     tags$h6("geom_point(aes(colour = class)) +", style = "padding: 0px 0px; margin-top:-2em"), br(), 
-                     tags$h6("(aes(label = model), data = best_in_class, ", style = "padding: 0px 0px; margin-top:-0.5em"),
+                     p("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:-1em"), br(), 
+                     p("geom_point(aes(colour = class)) +", style = "padding: 0px 0px; margin-top:-3em"), br(), 
+                     p("scale_y_continous(breaks = seq(15, 40, by = 5),", style = "padding: 0px 0px; margin-top:-3em"),
                      selectInput("labels", "labels =", choices = c("seq(15, 40, by = 5)","NULL"), selected = "seq(15, 40, by = 5)"),
-                     selectInput("legend_position", "legend.position =", choices = c("top","bottom", "right", "left", "none"), selected = "right"),
+                     p(") +", style = "padding: 0px 0px; margin-top:0em"),
+                     p("theme(", style = "padding: 0px 0px; margin-top:-1em"),
+                     selectInput("legend_position", "legend.position =", choices = c("top","bottom", "right", "left", "none"), selected = "right", width = 100),
+                     p(") +", style = "padding: 0px 0px; margin-top:0em"),
+                     p("scale_color_brewer(", style = "padding: 0px 0px; margin-top:-1em"),
                      selectInput("palette", "palette =", choices = c("YlGn","YlGnBu",
                                                                      "GnBu", "BuGn",
                                                                      "PuBuGn", "PuBu",
@@ -95,41 +104,54 @@ ui <- fluidPage(
                                                                      "Dark2", "Paired",
                                                                      "Pastel1", "Pastel2",
                                                                      "Set1", "Set2", "Set3"), 
-                                 selected = "Set1"),
-                     h6(")")
-                   ) #sidebarpanel
+                                 selected = "Set1", width = 100),
+                     p(") +", style = "padding: 0px 0px; margin-top:0em"),
+                     p("guides(color = guide_legend(", style = "padding: 0px 0px; margin-top:-1em"),
+                     numericInput("nrow", "nrow =", value = 7, width = 100),
+                     numericInput("size_guide", "size =", value = 1, width = 100),
+                     p(")", style = "padding: 0px 0px; margin-top:0em")
+                     ) #sidebarpanel
                ), #div
                mainPanel( 
-                 h6("¿Qué ocurre si seteas el argumento label como NULL?  http://colorbrewer2.org/ /n breaks` and `labels` must have the same length"),
+                 p("- 'guides' se le llama a los ejes más las leyendas", style = "padding: 0px 0px; margin-top:0em"), 
+                 p("- '`breaks` and `labels` must have the same length' = deben ser un número similar!" , style = "padding: 0px 0px; margin-top:-1em"), 
+                 p("- `scale_x_date()` sirve para eje x con fechas. Argumentos `date_labels` y `date_breaks`.", style = "padding: 0px 0px; margin-top:-1em"), 
+                 p("- POSICION: cambios de escala directo en los ejes, ejemplo: `scale_y_log10()`", style = "padding: 0px 0px; margin-top:-1em"),
+                 p("- COLOR: se puede usar la paleta viridis, ejemplo `scale_fill_viridis()`", style = "padding: 0px 0px; margin-top:-1em"),
+                 p("- COLOR: la diferencia entre fill y color es color de relleno o borde", style = "padding: 0px 0px; margin-top:-1em"),
+                 p("- COLOR: La terminación 'manual' para relacionarlo con valores, ejemplo: scale_color_manual()", style = "padding: 0px 0px; margin-top:-1em"),
                  plotOutput("ticks_legends")
                )# mainpanel
              ) #sidebarlayout
     ),#tabpanel
     tabPanel("Zooming",
              sidebarLayout(
-               div(style = "font-size: 10px; padding: 14px 0px; margin:0%",
-                   sidebarPanel(tags$h6("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:0em"), br(), 
-                                tags$h6("geom_point(aes(color = class)) +", style = "padding: 0px 0px; margin-top:-1em"), br(),
-                                tags$h6("geom_smooth(se = FALSE) +", style = "padding: 0px 0px; margin-top:-1em"), br(),   
+               div(style = "font-size: 13px; padding: 14px 0px; margin:0%",
+                   sidebarPanel(p("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:0em"),  
+                                p("geom_point(aes(color = class)) +", style = "padding: 0px 0px; margin-top:-1em"),
+                                p("geom_smooth(se = FALSE) +", style = "padding: 0px 0px; margin-top:-1em"),
+                                p("coord_cartesian(", style = "padding: 0px 0px; margin-top:-1em"),
                                 numericInput("xmin", "xmin =", value = 5),
                                 numericInput("xmax", "xmax =", value = 7),
                                 numericInput("ymin", "ymin =", value = 10),
                                 numericInput("ymax", "ymax =", value = 30),
-                                h6(")")
+                                p(")", style = "padding: 0px 0px; margin-top:-1em")
                    ) #sidebarpanel
                ), #div
                mainPanel(
-                 h5("There are three ways to control the plot limits: 1. Adjusting what data are plotted
-2. Setting the limits in each scale 3. Setting xlim and ylim in coord_cartesian()"),
+                 p("There are three ways to control the plot limits:", style = "padding: 0px 0px; margin-top:0em"),
+                 p("1. Adjusting what data are plotted", style = "padding: 0px 0px; margin-top:-1em"),
+                 p("2. Setting the limits in each scale" , style = "padding: 0px 0px; margin-top:-1em"),
+                 p("3. Setting xlim and ylim in coord_cartesian()", style = "padding: 0px 0px; margin-top:-1em"),
                  plotOutput("zoom"))#mainpanel
              ) #sidebarlayout
     ), #tabpanel
     tabPanel("Theme",
              sidebarLayout(
-                 div(style = "font-size: 10px; padding: 14px 0px; margin:0%",
-                     sidebarPanel(tags$h6("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:0em"), br(), 
-                                  tags$h6("geom_point(aes(color = class)) +", style = "padding: 0px 0px; margin-top:-1em"), br(),
-                                  tags$h6("geom_smooth(se = FALSE) +", style = "padding: 0px 0px; margin-top:-1em"), br(),   
+                 div(style = "font-size: 13px; padding: 14px 0px; margin:0%",
+                     sidebarPanel(p("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:0em"),  
+                                  p("geom_point(aes(color = class)) +", style = "padding: 0px 0px; margin-top:-1em"), 
+                                  p("geom_smooth(se = FALSE) +", style = "padding: 0px 0px; margin-top:-1em"), 
                                   selectInput("theme", "", choices = c("theme_classic()",
                                                                        "theme_light()",
                                                                        "theme_bw()",
@@ -143,20 +165,20 @@ ui <- fluidPage(
                                                                        "ggthemes::theme_stata()",
                                                                        "ggthemes::theme_excel_new()"),
                                                                         selected = "theme_gray()"),
-                                  h6(")")
+                                  p(")", style = "padding: 0px 0px; margin-top:0em")
                      ) #sidebarpanel
                  ), #div
                  mainPanel(
-                     h5("el paquete ggthemes te ofrece más posibilidades de temas"),
+                     p("el paquete ggthemes te ofrece más posibilidades de temas"),
                      plotOutput("plottheme"))#mainpanel
              ) #sidebarlayout
     ), #tabpanel
     tabPanel("Resize image",
              sidebarLayout(
-               div(style = "font-size: 10px; padding: 14px 0px; margin:0%",
-                   sidebarPanel(tags$h6("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:0em"), br(), 
-                                tags$h6("geom_point(aes(color = class)) +", style = "padding: 0px 0px; margin-top:-1em"), br(),
-                                tags$h6("geom_smooth(se = FALSE) +", style = "padding: 0px 0px; margin-top:-1em"), br(),   
+               div(style = "font-size: 13px; padding: 14px 0px; margin:0%",
+                   sidebarPanel(p("ggplot(mpg, aes(displ, hwy)) +", style = "padding: 0px 0px; margin-top:0em"), br(), 
+                                p("geom_point(aes(color = class)) +", style = "padding: 0px 0px; margin-top:-1em"), br(),
+                                p("geom_smooth(se = FALSE) +", style = "padding: 0px 0px; margin-top:-1em"), br(),   
                                 numericInput("height", "height =", value = 10),
                                 numericInput("width", "width =", value = 15),
                                 numericInput("dpi", "dpi =", value = 600),
@@ -168,6 +190,11 @@ ui <- fluidPage(
                  h5("Nota como cambian el tamaño de los puntos y las letras al cambiar el tamaño de salida de la imagen"),
                  imageOutput("myImage"))#mainpanel
              ) #sidebarlayout
+    ),#tabpanel
+    tabPanel("Links",
+             list(ui = fluidPage(
+               uiOutput("tab"))
+               )#list
     ) #tabpanel
     ) #navbarpage
 )#fluidpage
@@ -242,7 +269,7 @@ server <- function(input, output, session) {
                                  alpha = input$alpha,
                                  size = input$size,
                                  label.size = input$label_size,
-                                 #    segment.color = input$seg_col,
+                                 #segment.color = input$seg_col,
                                  hjust = input$hjust,
                                  vjust = input$vjust)
        
@@ -253,7 +280,7 @@ server <- function(input, output, session) {
             switch(input$graph,
                    "geom_text" = plot1(),
                    "geom_label" = plot2(),
-                   "geom_label_repel" = plot3()
+                   "ggrepel::geom_label_repel" = plot3()
             )
         })
         
@@ -351,7 +378,14 @@ server <- function(input, output, session) {
           if(input$legend_position == "right"){
             g <- g + theme(legend.position = "right")
           }
-         g
+         
+         g + guides(color = guide_legend(
+           nrow = input$nrow,
+           override.aes = list(size = input$size_guide))
+         )
+         
+         
+         
         })
      
         
@@ -395,7 +429,13 @@ server <- function(input, output, session) {
                  alt = "This is alternate text")
           }, deleteFile = TRUE)
           
-            
+          url <- a("Capítulo comunicación de gráficos", href="https://jrnold.github.io/r4ds-exercise-solutions/graphics-for-communication.html")
+          output$tab <- renderUI({
+            tagList("R4DS resuelto:", url)})
+          
+          url2 <- a("cheatsheet", href="https://jrnold.github.io/r4ds-exercise-solutions/graphics-for-communication.html")
+          output$tab2 <- renderUI({
+            tagList("ggplot2:", url2)})
         
 }
 
