fdf058da63044c73e864269c75dac969425ebb2a
Florencia D'Andrea <florencia.dandrea@gmail.com>
2020-05-22 14:39:45 -0300

    Update install.R

--- install.R
+++ install.R
@@ -4,3 +4,5 @@ install.packages(ggrepel)
 install.packages(shinythemes)
 install.packages(ggthemes)
 install.packages(RColorBrewer)
+install.packages(shiny)
+install.packages(httr) #dicen que es necesario para correr chinys en binder
