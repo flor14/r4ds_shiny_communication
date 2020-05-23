ee274b9701aa5129dc5dc7229c3faa28cf97cc9c
Florencia D'Andrea <florencia.dandrea@gmail.com>
2020-05-23 10:42:59 -0300

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,8 @@
-install.packages(dplyr)
-install.packages(ggplot2)
-install.packages(ggrepel)
-install.packages(shinythemes)
-install.packages(ggthemes)
-install.packages(RColorBrewer)
-install.packages(shiny)
-install.packages(httr) #dicen que es necesario para correr chinys en binder
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("ggrepel")
+install.packages("shinythemes")
+install.packages("ggthemes")
+install.packages("RColorBrewer")
+install.packages("shiny")
+install.packages("httr") #dicen que es necesario para correr chinys en binder
